90c040f39bf91a4a6480b21dcfc8563113e62d24
Mikoláš Pešek <mikolas.pesek@educanet.cz>
2026-01-26 15:02:11 +0100

    Ořezaný react na minimum distrakcí + Bazar app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import Header from "./components/Header.js"
-import Tasks from "./components/Tasks";
-import AddTask from "./components/AddTask";
 import {useState, useEffect} from "react";
+import BazarControlls from "./components/BazarControlls";
 import Home from "./pages/Home";
-import Todolist from "./pages/Todolist";
 
 import {Link, Routes, Route, Router} from "react-router";
 
 function App() {
     return (
-        <article>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/todo">To-do list</Link>
+        <article className="container">
+            <nav className="d-flex align-items-center">
+                <img src="https://i.guim.co.uk/img/media/c60c4f8ab60936ac7c1951cdc92b6be280c97ef2/0_645_4008_2772/master/4008.jpg?width=465&dpr=1&s=none&crop=none"
+                     alt=""
+                width="200px"/>
+                <h1>Párek bazar</h1>
             </nav>
+
+            <BazarControlls />
 
                 <Routes>
                     <Route path="/" element={<Home />}/>
-                    <Route path="/todo" element={<Todolist />}/>
                 </Routes>
         </article>
     )
